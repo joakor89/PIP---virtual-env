8bfcca8bddc9b0421fe5d2e3bfeaee56d174a0d6
Joaquin Romero Flores <jromerof30@gmail.com>
2023-01-16 12:19:05 -0700

    On web-server folder I've performed an FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,23 @@
 import store 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app =FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hello! Welcome to Aldebaran</h1>
+        <p>This is a paragraph</p>
+    """
 
 def run():
     store.get_categories()
     
 if __name__ == '__main__':
     run()
+    
